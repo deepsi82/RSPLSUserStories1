5efc81f478f42b2acbaee8d6977815fe537f6c15
deepsi82 <deepsi82@gmail.com>
2021-03-31 22:04:52 -0500

    getting result for playerOne

--- player.js
+++ player.js
@@ -2,31 +2,11 @@
 
 class Player{
     constructor(name){
-        this.score = 0;
         this.name = name;
+        this.score = 0;
         this.gesture = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
-
     }
-
-   
-
-
-
-
-}
-    roundBasedPlayerScore(){
-        let totalScore = 0;
-        for(i= 0; i < 3; i++){
-            totalScore += this.score;
-            }
-            return totalScore;
-    }
-    
-    
-
-
-}   
-
+} 
 class Human extends Player{
     constructor(name){
         super(name);
@@ -39,10 +19,8 @@ class Ai extends Player{
     }
 }  
 
-
-
-
-    
-
 module.exports = Player;
 console.log(module);
+
+
+
